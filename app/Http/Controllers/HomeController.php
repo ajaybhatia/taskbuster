24eb697f1ed3a6b7361ddc6d81511068335961ce
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-05-01 15:44:58 +0530

    Populate Home with data from DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
+use App\Task;
+
 class HomeController extends Controller
 {
     /**
@@ -24,6 +26,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tasks = Task::all();
+        return view('home', ['tasks' => $tasks]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,15 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            @foreach($tasks as $task)
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">{{ $task->name }}</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    {{ $task->description }}
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </div>
